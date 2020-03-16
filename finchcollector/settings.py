a16ef50073027566a79d6b41336d3a06c70948f4
Gabriel Russell <gabrielrussell@Gabriels-MacBook-Air.local>
2020-03-16 17:11:22 -0500

    adds base app functionality and routes

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,9 +75,9 @@ WSGI_APPLICATION = 'finchcollector.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'finchcollector',
     }
 }
 
---
+++ main_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Finch
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def finchs_index(request):
+    finchs = Finch.objects.all()
+    return render(request, 'finchs/index.html', {'finchs': finchs})
+
+def finchs_show(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finchs/show.html', { 'finch': finch })
